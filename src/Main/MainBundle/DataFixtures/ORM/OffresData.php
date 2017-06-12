df9a5d0343f6b1c844160ac75073fa7d5d21304f
l0ic68 <maxiloic@gmail.com>
2017-06-12 12:27:18 +0200

    fixtures

--- src/Main/MainBundle/DataFixtures/ORM/OffresData.php
+++ src/Main/MainBundle/DataFixtures/ORM/OffresData.php
@@ -14,12 +14,17 @@ class OffresData extends AbstractFixture implements OrderedFixtureInterface
     public function load(ObjectManager $manager)
     {
         $offre1 = new Offres();
-        $offre1->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nibh, blandit in iaculis sit amet, tincidunt elementum nunc. Morbi non placerat sapien, eu aliquet nibh. Curabitur iaculis augue in dapibus iaculis. Curabitur aliquet neque et augue maximus pharetra. Proin in sapien in diam tincidunt ullamcorper quis quis lacus. Nullam a elementum justo. Etiam nisi nisi, pharetra a luctus quis, porta nec ipsum. Quisque aliquet convallis nibh eu ultrices. Aenean elit augue, maximus vitae egestas posuere, venenatis eu elit. Fusce sed odio nulla.");
+        $offre1->setDescriptionPost("Du texte important de sous catégorie ou autre à completer ici, 70 a 150 caractères. Blablablablablablablablbalbalbalbalbalablabalabalblzndfisdsjiifdijfsjifijffjiefzefzfezjefzfçejefzjjfzez |");
+        $offre1->setDescriptionCompany("Ils sont super sympa mon gars");
+        $offre1->setDescriptionMission("imprimante");
+        $offre1->setDescriptionprofilesearch("Un humain");
+        $offre1->setTitle("Titre Offre 1");
+        $offre1->setCategory("Informatique");
+        $offre1->setWebsitelink("http://www.google.com");
 //        $offre1->setAuthor($this->getReference('user_test'));
         $offre1->setLocation("Strasbourg");
+        $offre1->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
         $offre1->setDateOffre(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre1->setDateOffre1(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre1->setDateOffre2(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
         $offre1->setName("Offre 1");
         $offre1->setPrice(75);
         $offre1->setDuration(75);
@@ -29,59 +34,80 @@ class OffresData extends AbstractFixture implements OrderedFixtureInterface
 
 
         $offre2 = new Offres();
-        $offre2->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nibh, blandit in iaculis sit amet, tincidunt elementum nunc. Morbi non placerat sapien, eu aliquet nibh. Curabitur iaculis augue in dapibus iaculis. Curabitur aliquet neque et augue maximus pharetra. Proin in sapien in diam tincidunt ullamcorper quis quis lacus. Nullam a elementum justo. Etiam nisi nisi, pharetra a luctus quis, porta nec ipsum. Quisque aliquet convallis nibh eu ultrices. Aenean elit augue, maximus vitae egestas posuere, venenatis eu elit. Fusce sed odio nulla.");
+        $offre2->setDescriptionPost("Du texte important de sous catégorie ou autre à completer ici, 70 a 150 caractères. Blablablablablablablablbalbalbalbalbalablabalabalblzndfisdsjiifdijfsjifijffjiefzefzfezjefzfçejefzjjfzez |");
 //        $offre2->setAuthor($this->getReference('user_test'));
         $offre2->setLocation("Paris");
+        $offre2->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+        $offre2->setDescriptionCompany("Ils sont super sympa mon gars");
+        $offre2->setDescriptionMission("imprimante");
+        $offre2->setDescriptionprofilesearch("Un humain");
+        $offre2->setTitle("Titre Offre 2");
+        $offre2->setCategory("commerce");
+        $offre2->setWebsitelink("http://www.google.com");
         $offre2->setName("Offre 2");
         $offre2->setPrice(75);
         $offre2->setDuration(75);
         $offre2->setDateOffre(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre2->setDateOffre1(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre2->setDateOffre2(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
         $offre2->setMedia($this->getReference('carouselMedia2'));
         $manager->persist($offre2);
 
 
 
         $offre3 = new Offres();
-        $offre3->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nibh, blandit in iaculis sit amet, tincidunt elementum nunc. Morbi non placerat sapien, eu aliquet nibh. Curabitur iaculis augue in dapibus iaculis. Curabitur aliquet neque et augue maximus pharetra. Proin in sapien in diam tincidunt ullamcorper quis quis lacus. Nullam a elementum justo. Etiam nisi nisi, pharetra a luctus quis, porta nec ipsum. Quisque aliquet convallis nibh eu ultrices. Aenean elit augue, maximus vitae egestas posuere, venenatis eu elit. Fusce sed odio nulla.");
+        $offre3->setDescriptionPost("Du texte important de sous catégorie ou autre à completer ici, 70 a 150 caractères. Blablablablablablablablbalbalbalbalbalablabalabalblzndfisdsjiifdijfsjifijffjiefzefzfezjefzfçejefzjjfzez |");
+        $offre3->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
 //        $offre3->setAuthor($this->getReference('user_test'));
         $offre3->setLocation("Allemagne");
+        $offre3->setDescriptionCompany("Ils sont super sympa mon gars");
+        $offre3->setDescriptionMission("imprimante");
+        $offre3->setDescriptionprofilesearch("Un humain");
+        $offre3->setTitle("Titre Offre 3");
+        $offre3->setCategory("Vente");
+        $offre3->setWebsitelink("http://www.google.com");
         $offre3->setName("Offre 3");
         $offre3->setPrice(75);
         $offre3->setDuration(75);
         $offre3->setDateOffre(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre3->setDateOffre1(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre3->setDateOffre2(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
+
         $offre3->setMedia($this->getReference('carouselMedia3'));
         $manager->persist($offre3);
 
 
 
         $offre4 = new Offres();
-        $offre4->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nibh, blandit in iaculis sit amet, tincidunt elementum nunc. Morbi non placerat sapien, eu aliquet nibh. Curabitur iaculis augue in dapibus iaculis. Curabitur aliquet neque et augue maximus pharetra. Proin in sapien in diam tincidunt ullamcorper quis quis lacus. Nullam a elementum justo. Etiam nisi nisi, pharetra a luctus quis, porta nec ipsum. Quisque aliquet convallis nibh eu ultrices. Aenean elit augue, maximus vitae egestas posuere, venenatis eu elit. Fusce sed odio nulla.");
+        $offre4->setDescriptionPost("Du texte important de sous catégorie ou autre à completer ici, 70 a 150 caractères. Blablablablablablablablbalbalbalbalbalablabalabalblzndfisdsjiifdijfsjifijffjiefzefzfezjefzfçejefzjjfzez |");
 //        $offre4->setAuthor($this->getReference('user_test'));
         $offre4->setLocation("Colmar");
+        $offre4->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
         $offre4->setName("Offre 4");
+        $offre4->setDescriptionCompany("Ils sont super sympa mon gars");
+        $offre4->setDescriptionMission("imprimante");
+        $offre4->setDescriptionprofilesearch("Un humain");
+        $offre4->setTitle("Titre Offre 4");
+        $offre4->setCategory("Test");
+        $offre4->setWebsitelink("http://www.google.com");
         $offre4->setPrice(75);
         $offre4->setDuration(75);
         $offre4->setDateOffre(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre4->setDateOffre1(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre4->setDateOffre2(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
         $offre4->setMedia($this->getReference('carouselMedia4'));
         $manager->persist($offre4);
 
 
         $offre5 = new Offres();
-        $offre5->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla felis nibh, blandit in iaculis sit amet, tincidunt elementum nunc. Morbi non placerat sapien, eu aliquet nibh. Curabitur iaculis augue in dapibus iaculis. Curabitur aliquet neque et augue maximus pharetra. Proin in sapien in diam tincidunt ullamcorper quis quis lacus. Nullam a elementum justo. Etiam nisi nisi, pharetra a luctus quis, porta nec ipsum. Quisque aliquet convallis nibh eu ultrices. Aenean elit augue, maximus vitae egestas posuere, venenatis eu elit. Fusce sed odio nulla.");
+        $offre5->setDescriptionPost("Du texte important de sous catégorie ou autre à completer ici, 70 a 150 caractères. Blablablablablablablablbalbalbalbalbalablabalabalblzndfisdsjiifdijfsjifijffjiefzefzfezjefzfçejefzjjfzez |Post");
+        $offre5->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
 //        $offre5->setAuthor($this->getReference('user_test'));
         $offre5->setLocation("Urschenheim");
+        $offre5->setDescriptionCompany("Ils sont super sympa mon gars");
+        $offre5->setDescriptionMission("imprimante");
+        $offre5->setDescriptionprofilesearch("Un humain");
+        $offre5->setTitle("Titre Offre 5");
+        $offre5->setCategory("Informatique");
+        $offre5->setWebsitelink("http://www.google.com");
         $offre5->setName("Offre 5");
         $offre5->setPrice(75);
         $offre5->setDuration(75);
         $offre5->setDateOffre(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre5->setDateOffre1(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
-        $offre5->setDateOffre2(new \DateTime("13-05-2017 00:00"), new \DateTimeZone('Europe/Paris'));
         $offre5->setMedia($this->getReference('carouselMedia5'));
         $manager->persist($offre5);
 
--- src/Main/MainBundle/DataFixtures/ORM/UserData.php
+++ src/Main/MainBundle/DataFixtures/ORM/UserData.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use User\UserBundle\Entity\User;
+use User\UserBundle\Entity\Candidat;
 
 class UserData extends AbstractFixture implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
 {
@@ -19,39 +20,47 @@ class UserData extends AbstractFixture implements FixtureInterface, ContainerAwa
 
     public function load(ObjectManager $manager)
     {
+        $discriminator = $this->container->get('pugx_user.manager.user_discriminator');
+        $discriminator->setClass('User\UserBundle\Entity\Candidat');
+
+        $userManager = $this->container->get('pugx_user_manager');
+
+        $user = $userManager->createUser();
         $role = array('ROLE_ADMIN');
 
-
-        $user_test = new User();
-        $user_test->setRoles($role);
-        $user_test->setEmail('test.test@test.test');
-        $user_test->setPassword($this->container->get('security.encoder_factory')->getEncoder($user_test)->encodePassword('test', $user_test->getSalt()));
-        $user_test->setEnabled(1);
-        $user_test->setMedia($this->getReference('ascampus'));
-        $manager->persist($user_test);
-
-        $user_client = new User();
-        $user_client->setRoles($role);
-        $user_client->setEmail('client@mail.fr');
-        $user_client->setPassword($this->container->get('security.encoder_factory')->getEncoder($user_client)->encodePassword('client', $user_client->getSalt()));
-        $user_client->setEnabled(1);
-        $user_client->setMedia($this->getReference('pictocollaboration'));
-        $user_client->setCandidat($this->getReference('candidat1'));
-        $manager->persist($user_client);
-
-        $user_entreprise = new User();
-        $user_entreprise->setEmail('entreprise@mail.fr');
-        $user_entreprise->setPassword($this->container->get('security.encoder_factory')->getEncoder($user_entreprise)->encodePassword('entreprise', $user_entreprise->getSalt()));
-        $user_entreprise->setEnabled(1);
-        $user_entreprise->setMedia($this->getReference('citybg'));
-        $user_entreprise->setEntreprise($this->getReference('entreprise1'));
-        $manager->persist($user_entreprise);
-
-        $manager->flush();
-
-        $this->addReference('user_test', $user_test);
-        $this->addReference('user_client', $user_client);
-        $this->addReference('user_entreprise', $user_entreprise);
+        $user->setRoles($role);
+        $user->setUsername('loic');
+        $user->setNom('loic');
+        $user->setPrenom('loic');
+        $user->setPhone('loic');
+        $user->setEmail('test.test@test.test');
+        $user->setPassword($this->container->get('security.encoder_factory')->getEncoder($user)->encodePassword('test', $user->getSalt()));
+        $user->setEnabled(1);
+//        $user->setMedia($this->getReference('ascampus'));
+        $userManager->updateUser($user);
+//
+//        $user_client = new User();
+//        $user_client->setRoles($role);
+//        $user_client->setEmail('client@mail.fr');
+//        $user_client->setPassword($this->container->get('security.encoder_factory')->getEncoder($user_client)->encodePassword('client', $user_client->getSalt()));
+//        $user_client->setEnabled(1);
+//        $user_client->setMedia($this->getReference('pictocollaboration'));
+//        $user_client->setCandidat($this->getReference('candidat1'));
+//        $manager->persist($user_client);
+//
+//        $user_entreprise = new User();
+//        $user_entreprise->setEmail('entreprise@mail.fr');
+//        $user_entreprise->setPassword($this->container->get('security.encoder_factory')->getEncoder($user_entreprise)->encodePassword('entreprise', $user_entreprise->getSalt()));
+//        $user_entreprise->setEnabled(1);
+//        $user_entreprise->setMedia($this->getReference('citybg'));
+//        $user_entreprise->setEntreprise($this->getReference('entreprise1'));
+//        $manager->persist($user_entreprise);
+//
+//        $manager->flush();
+//
+//        $this->addReference('user_test', $user_test);
+//        $this->addReference('user_client', $user_client);
+//        $this->addReference('user_entreprise', $user_entreprise);
     }
     public function getOrder()
     {
